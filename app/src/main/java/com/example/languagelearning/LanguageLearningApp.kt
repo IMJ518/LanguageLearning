8d4aafc22c615abb711b5b2d120a926a24811adc
leolourencini <leolourencini@hotmail.com>
2023-11-29 13:36:34 -0600

    Created bottom menu bar.

--- app/src/main/java/com/example/languagelearning/LanguageLearningApp.kt
+++ app/src/main/java/com/example/languagelearning/LanguageLearningApp.kt
@@ -1,11 +1,43 @@
 package com.example.languagelearning
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import com.example.languagelearning.data.DataSource
 import com.example.languagelearning.ui.SelectLanguageScreen
 import com.example.languagelearning.ui.FlashCardScreen
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -23,69 +55,130 @@ enum class Route {
     FlashCardFood
 }
 
+data class BottomNavigationItem(
+    val title: String,
+    val link: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LanguageLearningApp() {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            link = Route.LanguageSelection.name,
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home
+        ),
+        BottomNavigationItem(
+            title = "Favorites",
+            link = Route.LanguageSelection.name,
+            selectedIcon = Icons.Filled.Favorite,
+            unselectedIcon = Icons.Outlined.FavoriteBorder
+        ),
+        BottomNavigationItem(
+            title = "Add Flashcard",
+            link = Route.LanguageSelection.name,
+            selectedIcon = Icons.Filled.AddCircle,
+            unselectedIcon = Icons.Outlined.Add
+        )
+    )
+    var selectedItemIndex by rememberSaveable {
+        mutableIntStateOf(0)
+    }
     val navController = rememberNavController()
     val backStackEntry by navController.currentBackStackEntryAsState()
-
-    NavHost(
-        navController = navController,
-        startDestination = Route.LanguageSelection.name,
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ){}
+    Scaffold (
+        bottomBar = {
+            androidx.compose.material3.NavigationBar() {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selectedItemIndex == index, {
+                            selectedItemIndex = index
+                            navController.navigate(item.link)
+                        }, {
+                            Icon(
+                                imageVector = if(index == selectedItemIndex) {
+                                    item.selectedIcon
+                                } else item.unselectedIcon,
+                                contentDescription = item.title
+                            )
+                        },
+                        label = {
+                            Text(text = item.title)
+                        }
+                    )
+                }
+            }
+        }
     ) {
-        composable(route = Route.LanguageSelection.name) {
-            SelectLanguageScreen(
-                languageOptions = DataSource.languageCodes,
-                flags = DataSource.flags,
-                navController = navController
-            )
-        }
 
-        composable(
-            route = "${Route.CategorySelection.name}/{languageCode}",
-            arguments = listOf(
-                navArgument("languageCode"){
-                    type = NavType.StringType
-                }
-            )
+        NavHost(
+            navController = navController,
+            startDestination = Route.LanguageSelection.name,
         ) {
-            SelectCategoryScreen(
-                categoryOptions = DataSource.categoryNames,
-                categoryPhotos = DataSource.categoryPhotos,
-                languageCode = backStackEntry?.arguments?.getString("languageCode"),
-                navController = navController
-            )
-        }
+            composable(route = Route.LanguageSelection.name) {
+                SelectLanguageScreen(
+                    languageOptions = DataSource.languageCodes,
+                    flags = DataSource.flags,
+                    navController = navController
+                )
+            }
 
-        composable(
-            route = "${Route.FlashCard.name}/{languageCode}",
-            arguments = listOf(
-                navArgument("languageCode"){
-                    type = NavType.StringType
-                }
-            )
-        ) {
-            FlashCardScreen(
-                animalNames = DataSource.animalNames,
-                animalPhotos = DataSource.animalPhotos,
-                languageCode = backStackEntry?.arguments?.getString("languageCode"),
-                navController = navController
-            )
-        }
+            composable(
+                route = "${Route.CategorySelection.name}/{languageCode}",
+                arguments = listOf(
+                    navArgument("languageCode"){
+                        type = NavType.StringType
+                    }
+                )
+            ) {
+                SelectCategoryScreen(
+                    categoryOptions = DataSource.categoryNames,
+                    categoryPhotos = DataSource.categoryPhotos,
+                    languageCode = backStackEntry?.arguments?.getString("languageCode"),
+                    navController = navController
+                )
+            }
 
-        composable(
-            route = "${Route.FlashCardFood.name}/{languageCode}",
-            arguments = listOf(
-                navArgument("languageCode"){
-                    type = NavType.StringType
-                }
-            )
-        ) {
-            FlashCardFoodScreen(
-                foodNames = DataSource.foodNames,
-                foodPhotos = DataSource.foodPhotos,
-                languageCode = backStackEntry?.arguments?.getString("languageCode"),
-                navController = navController
-            )
+            composable(
+                route = "${Route.FlashCard.name}/{languageCode}",
+                arguments = listOf(
+                    navArgument("languageCode"){
+                        type = NavType.StringType
+                    }
+                )
+            ) {
+                FlashCardScreen(
+                    animalNames = DataSource.animalNames,
+                    animalPhotos = DataSource.animalPhotos,
+                    languageCode = backStackEntry?.arguments?.getString("languageCode"),
+                    navController = navController
+                )
+            }
+
+            composable(
+                route = "${Route.FlashCardFood.name}/{languageCode}",
+                arguments = listOf(
+                    navArgument("languageCode"){
+                        type = NavType.StringType
+                    }
+                )
+            ) {
+                FlashCardFoodScreen(
+                    foodNames = DataSource.foodNames,
+                    foodPhotos = DataSource.foodPhotos,
+                    languageCode = backStackEntry?.arguments?.getString("languageCode"),
+                    navController = navController
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/languagelearning/MainActivity.kt
+++ app/src/main/java/com/example/languagelearning/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.languagelearning
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.languagelearning.ui.theme.LanguageLearningTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
