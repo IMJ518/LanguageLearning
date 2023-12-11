52d8a477661044338d3ba1685866cdaf949586f3
leolourencini <leolourencini@hotmail.com>
2023-12-11 16:04:45 -0600

    Added title to the app.

--- app/src/main/java/com/example/languagelearning/ui/SelectCategoryScreen.kt
+++ app/src/main/java/com/example/languagelearning/ui/SelectCategoryScreen.kt
@@ -1,5 +1,6 @@
 package com.example.languagelearning.ui
 
+import android.app.UiModeManager
 import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Alignment
 
 import androidx.compose.ui.Modifier
@@ -71,5 +73,7 @@ fun SelectCategoryScreen(
         }
     }
 
-    BtnBack(onClick = { navController.navigateUp() })
+    BtnBack(onClick = { //navController.navigateUp()
+        
+    })
 }
